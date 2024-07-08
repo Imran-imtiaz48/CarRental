ca0a7efbe75da57667031168ae4e09b83ab1854d
Imran Imtiaz <imranimtiaz48@hotmail.com>
2024-07-08 06:34:21 +0400

    Update main.js
    Changes Made:
    1.	Consistent Naming Conventions: Changed variable names and class toggles to be more consistent and descriptive (opned to opened, ads_box to advancedSearchBox).
    2.	Code Structure: Ensured proper indentation and spacing for readability.
    3.	Event Parameter Naming: Used event instead of e for better clarity.

--- js/main.js
+++ js/main.js
@@ -1,41 +1,24 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
+    // Hamburger Menu & Icon
+    $('.hamburger').on('click', function(event) {
+        event.preventDefault();
+        $(this).toggleClass('opened');
+        $('header nav').toggleClass('active');
+    });
 
+    // Advanced Search Form & Icon
+    $('#advanced_search_btn').on('click', function(event) {
+        event.preventDefault();
+        var advancedSearchBox = $('.advanced_search');
 
-
-	/*  Hamburger Menu & Icon  */
-	$('.hamburger').on('click', function(e){
-		
-		e.preventDefault();
-		$(this).toggleClass('opned');
-		$('header nav').toggleClass('active');
-		
-	});
-
-
-
-
-	/*  Advanced search form & Icon  */
-	$('#advanced_search_btn').on("click", function(e){
-		e.preventDefault();
-
-		var ads_box =$('.advanced_search');
-		
-		if(!ads_box.hasClass('advanced_displayed')){
-
-			$(this).addClass('active');
-			ads_box.stop().fadeIn(200).addClass('advanced_displayed');
-
-		}else{
-
-			$(this).removeClass('active');
-			ads_box.stop().fadeOut(200).removeClass('advanced_displayed');
-
-		}
-
-	});
-
-
-
+        if (!advancedSearchBox.hasClass('advanced_displayed')) {
+            $(this).addClass('active');
+            advancedSearchBox.stop().fadeIn(200).addClass('advanced_displayed');
+        } else {
+            $(this).removeClass('active');
+            advancedSearchBox.stop().fadeOut(200).removeClass('advanced_displayed');
+        }
+    });
 
 });
